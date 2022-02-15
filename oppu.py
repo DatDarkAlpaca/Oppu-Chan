40ff720fb134bee4f0aa446f3ef7be2f3db89245
Alpaca <70183390+DatDarkAlpaca@users.noreply.github.com>
2022-02-14 20:54:30 -0400

    Add difficulty adjustment

--- oppu.py
+++ oppu.py
@@ -1,5 +1,5 @@
 from tkinter import filedialog as fd
-from random import choice
+from random import choices
 from tkinter import ttk
 from os import getcwd
 import tkinter as tk
@@ -24,17 +24,18 @@ class Oppu(tk.Tk):
         self.menu_bar = tk.Menu(self)
 
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.file_menu.add_command(label='Load', command=self.load_question)
+        self.file_menu.add_command(label='Open...', command=self.load_question, accelerator='Ctrl+O')
+        self.file_menu.add_command(label='Difficulty', command=self.select_difficulty)
         self.file_menu.add_separator()
-        self.file_menu.add_command(label='Exit', command=self.quit)
+        self.file_menu.add_command(label='Exit', command=self.quit, accelerator='Ctrl+E')
 
         self.menu_bar.add_cascade(label='File', menu=self.file_menu)
         self.config(menu=self.menu_bar)
 
         # Variables:
         self.questions = load_questions('questions/katakana.ans')
-        self.current_question = choice(list(self.questions.keys()))
-        self.streak = 0
+        self.current_question = ''.join(choices(list(self.questions.keys()), k=1))
+        self.streak, self.difficulty = 0, 1
 
         # Widgets:
         self.streak_label, self.correct_label, self.question_label, self.answer_button, self.text_input = [None] * 5
@@ -43,16 +44,17 @@ class Oppu(tk.Tk):
 
     # Button Commands:
     def answer_command(self, event=None):
-        if self.text_input.get().upper() == self.questions[self.current_question]:
-            self.correct_label.config(text=f"Correct!", fg='#228B22')
+        answer = ''.join([self.questions[x] for x in self.current_question])
 
-            self.streak += 1
+        if self.text_input.get().upper() == answer:
+            self.correct_label.config(text=f"Correct!", foreground='#228B22')
+
+            self.streak += self.difficulty
             self.streak_label.config(text=f"Streak: {self.streak}")
 
         else:
-            correct = self.questions[self.current_question].title()
-            self.correct_label.config(text=f"Incorrect... That was {correct}.",
-                                      fg='#ff3232')
+            self.correct_label.config(text=f"Incorrect... That was {answer.title()}.",
+                                      foreground='#ff3232')
 
             self.streak = 0
             self.streak_label.config(text=f"Streak: 0")
@@ -63,7 +65,7 @@ class Oppu(tk.Tk):
         self.change_question()
 
     def change_question(self):
-        self.current_question = choice(list(self.questions.keys()))
+        self.current_question = ''.join(choices(list(self.questions.keys()), k=self.difficulty))
         self.question_label.config(text=f"{self.current_question}")
 
     # Menu Commands:
@@ -73,6 +75,32 @@ class Oppu(tk.Tk):
         filename = fd.askopenfilename(title='Open a file', initialdir=getcwd(), filetypes=filetypes)
         self.questions = load_questions(filename)
         self.change_question()
+
+    def select_difficulty(self):
+        selector = tk.Toplevel()
+        selector.geometry("300x70")
+        selector.focus()
+
+        difficulty_label = ttk.Label(selector, text=f"{self.difficulty}")
+
+        def control_difficulty(value=None):
+            self.difficulty = round(float(value))
+            difficulty_label.config(text=f"{self.difficulty}")
+
+        scale = ttk.Scale(selector, from_=1, to=5, value=self.difficulty, orient=tk.HORIZONTAL,
+                          command=control_difficulty)
+
+        def on_ok():
+            self.change_question()
+            selector.destroy()
+
+        ok_button = ttk.Button(selector, text='Ok', command=on_ok)
+
+        scale.pack(fill='x')
+        difficulty_label.pack()
+        ok_button.pack()
+
+        self.difficulty = scale.get()
 
     # Helpers:
     def _set_icon(self):
